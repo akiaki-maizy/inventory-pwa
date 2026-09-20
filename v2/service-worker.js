9d27fdd30cace32e2e884de9be1b8bae421ee12c
akiaki-maizy <mild0803km.y@gmail.com>
2026-09-20 17:35:29 +0900

    Ver2: refresh shell for inventory filters

--- v2/service-worker.js
+++ v2/service-worker.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='inventory-pwa-v2-shell-20260920d';
+const CACHE='inventory-pwa-v2-shell-20260920g';
 const SHELL=[
   './','./index.html','./style.css','./manifest.webmanifest',
   './db.js','./inventory-service.js','./master-service.js','./backup-service.js','./migration-service.js',
