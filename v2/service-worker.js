1d9f572a31a713cd494b9abc80dfd21c02724074
akiaki-maizy <mild0803km.y@gmail.com>
2026-09-20 19:31:17 +0900

    Ver2: refresh shell for home and responsive UI

--- v2/service-worker.js
+++ v2/service-worker.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='inventory-pwa-v2-shell-20260920t2';
+const CACHE='inventory-pwa-v2-shell-20260920u';
 const SHELL=[
   './','./index.html','./style.css','./manifest.webmanifest',
   './db.js','./inventory-service.js','./master-service.js','./backup-service.js','./migration-service.js',
