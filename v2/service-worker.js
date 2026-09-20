635e45f767d518cd50cae2499d43f4cd536fe4bb
akiaki-maizy <mild0803km.y@gmail.com>
2026-09-20 17:56:57 +0900

    Ver2: refresh shell for history tools

--- v2/service-worker.js
+++ v2/service-worker.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='inventory-pwa-v2-shell-20260920h';
+const CACHE='inventory-pwa-v2-shell-20260920i';
 const SHELL=[
   './','./index.html','./style.css','./manifest.webmanifest',
   './db.js','./inventory-service.js','./master-service.js','./backup-service.js','./migration-service.js',
